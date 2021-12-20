649d7ac2540a474159502d82aafc28f4e4cc52ad
Shreesh Pandey <shvspandey@gmail.com>
2021-12-20 13:56:35 +0530

    Added :- Even or Odd Program

---
+++ EvenOdd.py
@@ -0,0 +1,8 @@
+class EvenOdd:
+    def even_odd(self):
+        number = int(input("Enter the Number"))
+        if number % 2 == 0:
+            print("Even Number")
+        else:
+            print("Odd Number")
+
