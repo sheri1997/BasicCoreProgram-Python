1a0b531094fa80527980d40261c7f221bd6ef4a2
Shreesh Pandey <shvspandey@gmail.com>
2021-12-20 14:03:58 +0530

    Added :- Vowel or Consonent Program

---
+++ VowelOrConsonent.py
@@ -0,0 +1,8 @@
+class VowelOrConsonent:
+    def vowel_or_consonent(self):
+        alphabet = input("Enter The Character")
+        if alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u' :
+            print("It Is A Vowel")
+        else:
+            print("It Is A Consonant")
+
